d1821f334bbb20705d7a6e07c606165821cfafaf
kareemtarekK <engkareemtarek555@gmail.com>
2025-04-20 17:42:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const messages = [
+  "Learning React is fun and powerful! ⚛️",
+  "Your progress has been saved successfully 💾",
+  "Keep building, you're doing great! 🚀",
+];
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">{messages[step - 1]}</p>
+
+      <div className="buttons">
+        <button className="active" onClick={handlePrevious}>
+          Previous
+        </button>
+        <button className="active" onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
