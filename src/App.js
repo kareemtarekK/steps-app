491624bb381e22307b8437ea1b60dcb4a5fb84c8
kareemtarekK <engkareemtarek555@gmail.com>
2025-04-26 01:22:32 +0300

    add close button && and make compare with vanilla JS code with React code

--- src/App.js
+++ src/App.js
@@ -7,32 +7,42 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevious = () => {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   };
   const handleNext = () => {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">{messages[step - 1]}</p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button className="active" onClick={handlePrevious}>
-          Previous
-        </button>
-        <button className="active" onClick={handleNext}>
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button className="active" onClick={handlePrevious}>
+              Previous
+            </button>
+            <button className="active" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
